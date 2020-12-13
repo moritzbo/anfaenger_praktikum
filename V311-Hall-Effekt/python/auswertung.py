892e2974ca1af95f3910c88903fb9aefab702445
Moritz Bosse <moritz.bosse@tu-dortmund.de>
2020-12-13 18:54:00 +0100

    new stuff

--- V311-Hall-Effekt/python/auswertung.py
+++ V311-Hall-Effekt/python/auswertung.py
@@ -3,10 +3,30 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
 from scipy.stats import sem
+import scipy.constants as const
 
 
-B1 = ufloat(1244.7, 1)
-B2 = ufloat(1230.1, 1)
+B = ufloat(1230.1, 1)
+e = const.physical_constants['elementary charge']
+d = 105.2*(10**(-6))
+print(d)
+print(e[0])
 
-B_mean = (B1 + B2)/2
-print(B_mean)
+I, U = np.genfromtxt("../Daten/daten_hall_bconst_pos.txt", unpack=True)
+
+U *= 10**(-3)
+
+params, covar_matrix = np.polyfit(I, U, deg= 1, cov=True)
+
+errors = np.sqrt(np.diag(covar_matrix))
+
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value:.8f} ± {error:.8f}')
+
+A = ufloat(params[0], errors[0])
+
+n = B/(A*e[0]*d)
+print(f"n: {n}")
+
+print(I)
+print(U)
