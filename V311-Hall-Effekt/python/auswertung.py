43a40c10637a4306c4b1dc422c728a7f9117104f
Moritz Bosse <moritz.bosse@tu-dortmund.de>
2020-12-14 17:14:52 +0100

    merge

--- V311-Hall-Effekt/python/auswertung.py
+++ V311-Hall-Effekt/python/auswertung.py
@@ -6,9 +6,25 @@ from scipy.stats import sem
 import scipy.constants as const
 
 
-B = ufloat(1230.1, 1)
+B = ufloat(1.2301, 1*10**(-3))
 e = const.physical_constants['elementary charge']
 d = 105.2*(10**(-6))
+rho = 8920
+m_cuatom = 63.4 * 10**(-27)
+n_st = rho/m_cuatom
+j = 10**6
+
+m_0 = 9.109 * 10**(-31)
+R= ufloat(2.60, 0.13)
+#zu ÄNDERNDE WERTE
+b = ufloat(0.025, 0.001)
+L = ufloat(0.04, 0.001)
+
+Q = b*d
+
+
+
+
 print(d)
 print(e[0])
 
@@ -24,9 +40,21 @@ for name, value, error in zip('ab', params, errors):
     print(f'{name} = {value:.8f} ± {error:.8f}')
 
 A = ufloat(params[0], errors[0])
+print(f'A: {A}')
 
 n = B/(A*e[0]*d)
 print(f"n: {n}")
 
 print(I)
 print(U)
+
+z = n/n_st
+
+
+tau = (2*m_0*L)/((e[0])**2 *n *R * Q)
+drift = - j/(n*e[0])
+
+print(f'tau: {tau}')
+print(f'z: {z}')
+print(f'drift: {drift}')
+
