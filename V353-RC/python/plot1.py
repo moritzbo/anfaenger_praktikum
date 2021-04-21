d28df8d49ac4c3d1c8230bbfab5da4150df4d7ed
Philipp <philipp.zolthoff@web.de>
2021-04-21 12:44:01 +0200

    base

--- V353-RC/python/plot1.py
+++ V353-RC/python/plot1.py
@@ -7,23 +7,29 @@ import scipy.constants as const
 import uncertainties.unumpy as unp
 
 U, t = np.genfromtxt("../data/ab.dat", unpack=True)
-
-plt.plot(U, 
+ulog = np.log(U)
+plt.plot(ulog, 
         t,
         'bx',
         label='entladen',
         linewidth=1.5)
 
-params, covar_matrix = np.polyfit(U, t, deg= 1, cov=True)
+
+Uneu = ulog[:-1]
+tneu = t[:-1]
+print(U)
+print(Uneu)
+params, covar_matrix = np.polyfit(Uneu, tneu, deg= 1, cov=True)
 
 errors = np.sqrt(np.diag(covar_matrix))
 
 for name, value, error in zip('ab', params, errors):
     print(f'{name} = {value:.3f} ± {error:.3f}')
-x = np.linspace(0, 2)
+x = np.linspace(-3, 2)
 plt.plot(x, 
         params[0]*x + params[1],
         'k--',
         label='Lineare Ausgleichsgerade',
         linewidth=1.5)
+#plt.yscale("log")
 plt.show()
