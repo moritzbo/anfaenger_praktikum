53579ea7b306b5b99ded4b3519d6a9c2ed03c623
Philipp <philipp.zolthoff@web.de>
2021-05-11 16:41:03 +0200

    nikolai raus mit der nase

---
+++ V204-Wärmeleitung/python/plot3.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+from scipy.stats import sem
+import scipy.constants as const
+import uncertainties.unumpy as unp
+
+
+n, a, b, c, d, e, f, g, h = np.genfromtxt("../data/statisch.dat", unpack =True)
+
+plt.plot(n/5, (g-h), 'b-', label="T7 - T8", markersize=1)
+plt.plot(n/5, (b-a), 'k-', label="T2 - T1", markersize=1)
+
+plt.grid()
+plt.legend()
+plt.show()
