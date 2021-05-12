91e4989607c9f3e11331ecc7ef24f4d4b44b46ec
Philipp <philipp.zolthoff@web.de>
2021-05-12 15:43:10 +0200

    merge

---
+++ V204-Wärmeleitung/python/plot7.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+from scipy.stats import sem
+import scipy.constants as const
+import uncertainties.unumpy as unp
+
+
+n, a, b, c, d, e, f, g, h = np.genfromtxt("data/dynamisch2.txt", unpack =True)
+
+
+plt.plot(n/2, e, 'b-', label="T5 Aluminium", markersize=1)
+plt.plot(n/2, f, 'k-', label="T6 Aluminium", markersize=1)
+
+plt.grid()
+plt.legend()
+
+plt.ylabel(r"$T[\si{\degreeCelsius}]$")
+plt.xlabel(r"$t[\si{\second}]")
+
+plt.savefig("build/plot6.pdf")
