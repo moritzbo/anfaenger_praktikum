ebdd304801ad51340ee5af7440d4ba88efbe5437
Philipp <philipp.zolthoff@web.de>
2021-05-12 10:38:14 +0200

    niklai bruh

--- V204-Wärmeleitung/python/plot6.py
+++ V204-Wärmeleitung/python/plot6.py
@@ -10,8 +10,8 @@ import uncertainties.unumpy as unp
 n, a, b, c, d, e, f, g, h = np.genfromtxt("data/dynamisch2.txt", unpack =True)
 
 
-plt.plot(n/2, a, 'b-', label="T1 Messing", markersize=1)
-plt.plot(n/2, b, 'k-', label="T2 Messing", markersize=1)
+plt.plot(n/2, g, 'b-', label="T7 Edelstahl", markersize=1)
+plt.plot(n/2, h, 'k-', label="T8 Edelstahl", markersize=1)
 
 plt.grid()
 plt.legend()
@@ -19,4 +19,4 @@ plt.legend()
 plt.ylabel(r"$T[\si{\degreeCelsius}]$")
 plt.xlabel(r"$t[\si{\second}]")
 
-plt.savefig("build/plot4.pdf")
+plt.savefig("build/plot5.pdf")
