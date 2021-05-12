7ce76bde9e42655fd347aa68aa415a4c98a30c18
Moritz Bosse <moritz.bosse@tu-dortmund.de>
2021-05-12 13:07:50 +0200

    change

--- V204-Wärmeleitung/python/rechnen.py
+++ V204-Wärmeleitung/python/rechnen.py
@@ -9,6 +9,9 @@ import uncertainties.unumpy as unp
 
 a1, a2, t = np.genfromtxt("../data/rechnen.txt", unpack =True)
 
+a1 = a1 
+
+a2 = a2
 
 
 meana1 = np.mean(a1)
@@ -55,6 +58,45 @@ kappamessing = dichtemessing * cmessing* (x)**2 /(2*tufloat * unp.log((a2ufloat)
 test = unp.log((a2ufloat)/(a1ufloat))
 
 print(test)
-print(f"{kappamessing:4f}")
+print(f"{kappamessing:.6f}")
 
 
+
+
+a7, a8, t2 = np.genfromtxt("../data/rechnen2.txt", unpack =True)
+
+
+meana7 = np.mean(a7)
+
+meana8 = np.mean(a8)
+
+meant2 = np.mean(t2)
+
+
+mean7err = sem(a7)
+mean8err = sem(a8)
+meant2err = sem(t2)
+
+
+t2ufloat = ufloat(meant2, meant2err)
+a7ufloat = ufloat(meana7, mean7err)
+a8ufloat = ufloat(meana8, mean8err)
+print("||||||||")
+print("||||||||")
+print(meana7)
+print(meana8)
+print(meant2)
+print("||||||||")
+print(mean7err)
+print(mean8err)
+print(meant2err)
+
+kappaedel = dichteedel * cedel * (x)**2 /(2*t2ufloat * unp.log((a7ufloat)/(a8ufloat)))
+
+
+print(f"{kappaedel:.6f}")
+
+
+test2 = unp.log((a7ufloat)/(a8ufloat))
+
+print(test2)
