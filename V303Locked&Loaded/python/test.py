6b05df8344d1a016a74d86c4b7424b562ea990c6
Moritz Bosse <moritz.bosse@tu-dortmund.de>
2021-06-30 12:07:11 +0200

    change

--- V303Locked&Loaded/python/test.py
+++ V303Locked&Loaded/python/test.py
@@ -6,23 +6,9 @@ from scipy.stats import sem
 import scipy.constants as const
 import uncertainties.unumpy as unp
 
-phi, U = np.genfromtxt("data/phaseOHNEE.dat", unpack=True)
+phi, U = np.genfromtxt("../data/phaseOHNEE.dat", unpack=True)
 
 # phiM, UM = np.genfromtxt("data/phaseMIT.dat", unpack=True)
-
-plt.plot(phi,
-        U,
-        "kx",
-        label="Messwerte OHNE",
-        linewidth=1.5)
-
-# plt.plot(phiM,
-#         UM,
-#         "k-",
-#         label="Messwerte MIT",
-#         linewidth=1.5)
-
-
 def sigmoid(phi, a, b, c, d):
    return a*np.cos((np.deg2rad(phi)*b+c))+d
  
@@ -34,16 +20,23 @@ uncertainties = np.sqrt(np.diag(covariance_matrix))
 for name, value, uncertainty in zip('abcd', params, uncertainties): 
     print(f'{name} = {value:.4f} ± {uncertainty:.4f}')
 
-# ABSTELLPLATZ:             np.deg2rad    , p0=(-94, 1, -50, 0) , p0=(150, 1, -50, 0)
+plt.plot(phi,
+        U,
+        "kx",
+        label="Messwerte OHNE",
+        linewidth=1.5)
 
 x = np.linspace(0,350)
+plt.plot(x-20, 
+        (-94.671*np.cos((np.deg2rad(x)*0.807-16.500)) -4.382),
+        'b-',
+        label='Lineare Ausgleichsgerade',
+        linewidth=1.5)
+
 plt.plot(x, 
         (params[0]*np.cos((np.deg2rad(x)*params[1]+params[2])) + params[3]),
         'b-',
         label='Lineare Ausgleichsgerade',
         linewidth=1.5)
 
-plt.legend()
-plt.savefig("build/plot1.pdf")
-
-#leerzeile
+plt.show()
