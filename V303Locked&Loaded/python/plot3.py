6ca8ff1308a0aea3e865995efd2c15b116dd12f2
Philipp <philipp.zolthoff@web.de>
2021-06-30 11:53:43 +0200

    balblablab

---
+++ V303Locked&Loaded/python/plot3.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+from scipy.stats import sem
+import scipy.constants as const
+import uncertainties.unumpy as unp
+
+d, U = np.genfromtxt("data/licht.dat", unpack=True)
+d=d*10**(-3) 
+plt.plot(d,
+        U,
+        "kx",
+        label="Messwerte",
+        linewidth=1.5)
+
+
+def sigmoid(d, a, b, c):
+   return a* 1/(d**b) + c 
+ 
+ 
+params, covariance_matrix = curve_fit(sigmoid, d, U)
+
+uncertainties = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, uncertainty in zip('abc', params, uncertainties): 
+    print(f'{name} = {value:.4f} ± {uncertainty:.4f}')
+
+x = np.linspace(0,0.52)
+plt.plot(x, 
+        params[0]*1/(x**params[1])+params[2],
+        'b-',
+        label='Ausgleichsfunktion',
+        linewidth=1.5)
+
+plt.legend()       
+# plt.show() 
+plt.savefig("build/plot3.pdf")
